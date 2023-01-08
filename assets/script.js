bd129737fad882712688e75b60ce9581ce6cb410
jwm <jwm3926@gmail.com>
2023-01-08 11:04:26 -0500

    finished functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
-const searchBox = document.getElementById('cityInput');
-const submitBtn = document.getElementById('cityInputButton');
-const apiKey = 'efc693abd192802e32ec5e23919e5afe'
+const searchBox = document.querySelector('#cityInput');
+const submitBtn = document.querySelector('#cityInputButton');
+const currentWeather = document.querySelector('#current')
+const currentWeatherIcon = document.querySelector('#icon-current')
+const temperature = document.querySelector('#tempurate')
+const humidity = document.querySelector('#humidity')
+const wind = document.querySelector('#wind')
+const uv = document.querySelector('#uv')
 
 const formSubmitHandler = (event) => {
     event.preventDefault();
@@ -15,7 +20,7 @@ const formSubmitHandler = (event) => {
 }
 
 const getCoordinates = (city) => {
-    let api = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
+    let api = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=ab9f65dea451c4b7f6c92a9815fed957";
     fetch(api)
         .then((res) => {
             console.log(res.json)
@@ -32,7 +37,7 @@ const getCoordinates = (city) => {
 }
 
 let getWeather = (lat, lon) => {
-    let api = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+    let api = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=ab9f65dea451c4b7f6c92a9815fed957";
     fetch(api)
         .then((res, err) => {
             if (res) {
@@ -46,6 +51,29 @@ let getWeather = (lat, lon) => {
             showWeather(data);
             showForecast(data);
         })
+};
+
+let showWeather = (data) => {
+    let api = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.lat + "&lon=" + data.lon + "&units=imperial&appid=ab9f65dea451c4b7f6c92a9815fed957";
+    let icon = "https://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png"
+    fetch(api)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            currentWeatherIcon.innerHTML = '<img src=' + icon + '>';
+        })
+
+        wind.textcontent = 'Wind speed is ' + data.current.wind_speed + "miles per hour"
+        temperature.textcontent = 'It is ' + data.current.temp + "degrees outside"
+        humidity.textContent = 'Humidity will be up to ' + data.current.humidity + '%'
 }
 
+let showForecast = (data) => {
+    for (i=1; i<6; i++) {
+        let current = document.querySelector('day' + i + '-title');
+        let forecast = document.querySelector("#card" + i);
+        forecast.classList.remove("d-none")
+    }
+}
 submitBtn.addEventListener('click', formSubmitHandler)
